4b60f45612c356a9c98746602327978ff9c5e6cd
Libor Michálek <libor@kvikymart.cz>
2022-05-05 15:09:03 +0200

    [src] Fix displaying questions with number less than 10

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -1,23 +1,25 @@
-import titles from './titles'
+import titles from './titles';
 
-const SideMenu = ({ section, subSection, setNumber }) => (
-  <div className="col-3">
-    <div
-      className="nav flex-column nav-pills"
-      role="tablist"
-      aria-orientation="vertical"
-    >
-      {titles[section][subSection].map((t, i) => (
-        <a
-          className="nav-link border-bottom border-right"
-          role="tab"
-          onClick={() => setNumber(i + 1)}
+const SideMenu = ({section, subSection, setNumber}) => (
+    <div className="col-3">
+        <div
+            className="nav flex-column nav-pills"
+            role="tablist"
+            aria-orientation="vertical"
         >
-          {t}
-        </a>
-      ))}
+            {titles[section][subSection].map((t, i) => (
+                <a
+                    className="nav-link border-bottom border-right"
+                    role="tab"
+                    onClick={() => setNumber((i + 1).toLocaleString(undefined, {
+                        minimumIntegerDigits: 2,
+                    }))}
+                >
+                    {(i + 1)}. {t}
+                </a>
+            ))}
+        </div>
     </div>
-  </div>
-)
+);
 
-export default SideMenu
+export default SideMenu;
